b8b7a103217a0acad4d246a45b25665a0fa23eac
Letenyei Ádám <letenyeidam89@gmail.com>
2020-12-21 17:01:04 +0100

    Controller
    Basic user interface ready -> Gui skeleton created,
    Quit function ready

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,45 @@
-public class Controller {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.TableView;
 
+public class Controller {
+    @FXML
+    private ScrollBar scrollbarChangeAuthor;
+
+    @FXML
+    private Label labelActualAuthor;
+
+    @FXML
+    private Label labelActualAlbum;
+
+    @FXML
+    private TableView<?> tableTrackList;
+
+    @FXML
+    private Button buttonPlay;
+
+    @FXML
+    private Button buttonPause;
+
+    @FXML
+    private Button buttonStop;
+
+    @FXML
+    private Button buttonPreviousSong;
+
+    @FXML
+    private Button buttonNextSong;
+
+    @FXML
+    private Button buttonExit;
+
+
+    @FXML
+    public void quit(ActionEvent e) {
+        Platform.exit();
+    }
 }
